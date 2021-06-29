aa7c522925565c22df75a4da58ef45c3709519ad
Vinnikatha <71521595+Vinnikatha@users.noreply.github.com>
2021-06-29 21:41:31 +0530

    Update Check_Leap_Year.java

--- Check_Leap_Year.java
+++ Check_Leap_Year.java
@@ -1,3 +1,4 @@
+/* write a java program to read a number and print it is a leap year or not */
 import java.util.Scanner;
 public class Check_Leap_Year 
 {
@@ -33,3 +34,15 @@ public class Check_Leap_Year
         }
     }
 }
+Output:
+C:\Users\stan>javac Check_Leap_Year.java
+
+C:\Users\stan>java Check_Leap_Year
+Enter any year:2000
+Year 2000 is a Leap Year
+
+C:\Users\stan>javac Check_Leap_Year.java
+
+C:\Users\stan>java Check_Leap_Year
+Enter any year:2015
+Year 2015 is not a Leap Year
