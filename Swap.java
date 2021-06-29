b2a7a9549c58c5427c2597e5928071fddcc9e29f
Vinnikatha <71521595+Vinnikatha@users.noreply.github.com>
2021-06-29 20:05:19 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -22,4 +22,23 @@ class Swap
    }
 
 
+output:
+C:\Users\stan>javac Swap.java
+
+C:\Users\stan>java Swap
+Enter the value of x and y
+20
+30
+before swapping numbers: 20
+after swapping: 30 20
+
+C:\Users\stan>javac Swap.java
+
+C:\Users\stan>java Swap
+Enter the value of x and y
+35
+40
+before swapping numbers: 35
+after swapping: 40 35
+
  
