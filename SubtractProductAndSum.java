7eaf6341408a576c8423e5a24881cfd3c4929d25
Vinnikatha <71521595+Vinnikatha@users.noreply.github.com>
2021-06-30 20:16:45 +0530

    Add files via upload

---
+++ SubtractProductAndSum.java
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        ///first keep track of a sum
+        int sum = 0;
+        int copy = n;
+        while (copy > 0) {
+            sum += copy % 10;
+            copy /= 10;
+        }
+        int prod = 1;
+        copy = n;
+        while(copy > 0) {
+            prod *= copy %10;
+            copy /= 10;
+        }
+        return prod - sum;
+    }
+};
+
+
+Output:
+Your input
+234
+Output
+15
+Expected
+15
