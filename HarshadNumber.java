54793d25e6bbcb9ed04601f80d850bb931f0e189
Vinnikatha <71521595+Vinnikatha@users.noreply.github.com>
2021-06-30 23:15:19 +0530

    Update HarshadNumber.java

--- HarshadNumber.java
+++ HarshadNumber.java
@@ -1,4 +1,4 @@
-/* Program to print given number is Harshad number or not */
+/*Example1 for java Program to print given number is Harshad number or not */
 import java.util.Scanner;
 public class HarshadNumber {
  
@@ -35,8 +35,40 @@ C:\Users\stan>java HarshadNumber
 Please enter number to be tested:156
 156 is a Harshad Number
 
-C:\Users\stan>javac HarshadNumber.java
 
-C:\Users\stan>java HarshadNumber
+/* Example2 for java Program to print given number is Harshad number or not */
+import java.util.Scanner;
+public class HarshadNumberOrNot {
+ 
+  public static void main(String args[]) {
+ 
+    //Input number to be tested
+    System.out.print("Please enter number to be tested:");
+    Scanner scanner = new Scanner(System.in);
+    Long inputNo = scanner.nextLong();
+ 
+    //Calculate sum of digits of inputNo
+    long temp = inputNo;
+    int sumOfDigits = 0;
+    while (temp > 0) {
+      long rem = temp % 10;
+      sumOfDigits += rem;
+      temp = temp / 10;
+    }
+ 
+    //Check if inputNo is divisible by sum of its digits
+    if (inputNo % sumOfDigits == 0) {
+      System.out.println(inputNo + " is a Harshad Number");
+    } else {
+      System.out.println(inputNo + " is NOT a Harshad Number");
+    }
+ 
+  }
+}
+
+Output:
+C:\Users\stan>javac HarshadNumberOrNot.java
+
+C:\Users\stan>java HarshadNumberOrNot
 Please enter number to be tested:22
 22 is NOT a Harshad Number
