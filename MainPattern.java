caab05eed5e313fe81c057af47ee09b86f946a72
Vinnikatha <71521595+Vinnikatha@users.noreply.github.com>
2021-07-01 21:19:29 +0530

    Add files via upload

---
+++ MainPattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class MainPattern
+{
+    public static void main(String[] args)
+    {
+        int n;
+    Scanner sc = new Scanner(System.in);
+  n = sc.nextInt();
+ 
+    for (int i=n; i>=1 ; i--)
+    {
+    for (int j=1; j<=i; j++)
+    {
+    System.out.print("* ");
+    }
+    System.out.println();
+       }
+     }
+  }
+
+
+Output:
+C:\Users\stan>javac MainPattern.java
+
+C:\Users\stan>java MainPattern
+5
+* * * * *
+* * * *
+* * *
+* *
+*
